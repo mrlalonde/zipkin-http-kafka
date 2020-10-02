cff9be84416a8fa8f8f44ec8bff9f2a4a0855762
Mathieu Lalonde <mrlalonde@live.ca>
2020-10-02 14:41:27 -0400

    Hacked it so it simply logs all the span

--- src/main/java/com/github/mrlalonde/zipkinwebbridge/ZipkinWebBridgeController.java
+++ src/main/java/com/github/mrlalonde/zipkinwebbridge/ZipkinWebBridgeController.java
@@ -3,7 +3,6 @@ package com.github.mrlalonde.zipkinwebbridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -11,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import zipkin2.Span;
+import zipkin2.codec.SpanBytesDecoder;
 
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -19,22 +21,19 @@ public class ZipkinWebBridgeController {
     private static final Logger LOG = LoggerFactory.getLogger(ZipkinWebBridgeController.class);
 
     private final String topic;
-    private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
-    ZipkinWebBridgeController(@Value("spans.topic") String topic, KafkaTemplate<String, byte[]> kafkaTemplate) {
+    ZipkinWebBridgeController(@Value("spans.topic") String topic) {
         this.topic = topic;
-        this.kafkaTemplate = kafkaTemplate;
     }
 
 
     @PostMapping("/api/v2/spans")
     public Mono<ResponseEntity<Object>> uploadSpans(@RequestBody byte[] spanBytes) {
-        return Mono.fromFuture(publish(spanBytes))
-                .map(ignoredSuccess -> ResponseEntity.ok().build())
-                .onErrorReturn(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
-    }
 
-    private CompletableFuture<SendResult<String, byte[]>> publish(byte[] spanBytes) {
-        return kafkaTemplate.send(topic, spanBytes).completable();
+        Collection<Span> spans = SpanBytesDecoder.JSON_V2.decodeList(spanBytes);
+        spans.forEach( span ->  LOG.info("receivedSpan: {}", span));
+
+
+        return Mono.just(ResponseEntity.ok().build());
     }
 }
